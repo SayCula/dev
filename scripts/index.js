3ff5c28d42d39784859c2f8937dd0f99562cb436
SayCula <98538872+SayCula@users.noreply.github.com>
2023-12-18 03:28:57 +0100

    up

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('nav');
+
+menu.innerHTML = `
+    <ul class="Header">
+        <li><a href="">CubifyCraft</a></li>
+        <li><a href="">Home</a></li>
+        <li><a href="">Downloads</a></li>
+        <li><a href="contact.html">Contact Us</a></li>
+        <li><a href="privacy.html">Privacy Policy</a></li>
+        <li><button id="btnLightMod">LightMod</button></li>
+    </ul>
+`;
